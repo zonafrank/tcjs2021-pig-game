9e21f9e3c6c1cc3fb45ea4b30330c6e7c755f65f
Echezona <echezona.oji@gmail.com>
2021-09-29 13:12:13 +0100

    change winning score to 100

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-const winningScore = 20;
+const winningScore = 100;
 let currentScore = 0;
 let currentPlayer = 1;
 const totalScores = {
